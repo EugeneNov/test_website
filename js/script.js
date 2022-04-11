628d92c7e5986d1449cc789347c4064fac7dcc56
Eugene <kanvoir.work@gmail.com>
2022-04-11 16:52:49 +0300

    Im done the lesson

--- js/script.js
+++ js/script.js
@@ -15,11 +15,43 @@
 "use strict";
 
 const movieDB = {
-	movies: [
-		"Логан",
-		"Лига справедливости",
-		"Ла-ла лэнд",
-		"Одержимость",
-		"Скотт Пилигрим против...",
-	],
-};
+		movies: [
+			"Логан",
+			"Лига справедливости",
+			"Ла-ла лэнд",
+			"Одержимость",
+			"Скотт Пилигрим против...",
+		],
+		removeAd: () => {
+			adBlocks.forEach((item) => {
+				item.remove();
+			});
+		},
+		changeGanre: () => {
+			promoGanre.textContent = "драма";
+		},
+		chengePromoImg: () => {
+			promoPoster.style.backgroundImage = "url(../img/bg.jpg)";
+		},
+		changePromoFilms: () => {
+			movieDB.movies.sort();
+			for (let i = 0; i < promoListFilms.length; i++) {
+				promoListFilms[i].innerHTML = `${i + 1}. ${
+					movieDB.movies[i]
+				} <div class="delete"></div>`;
+				// domListElement.textContent = `${i + 1}. ${movieDB.movies[i]}`;
+				// domListElement.insertAdjacentHTML(
+				// 	"beforeend",
+				// 	'<div class="delete"></div>'
+				// );
+			}
+		},
+	},
+	adBlocks = document.querySelectorAll(".promo__adv > img"),
+	promoPoster = document.querySelector("div.promo__bg"),
+	promoListFilms = document.querySelectorAll(".promo__interactive-item"),
+	promoGanre = document.querySelector(".promo__genre");
+movieDB.removeAd();
+movieDB.changeGanre();
+movieDB.chengePromoImg();
+movieDB.changePromoFilms();
